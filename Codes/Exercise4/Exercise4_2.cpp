589d6d183d9782f9f4f5ea2f9f26a447e6ae3042
Stefano Ghislandi <stefano.ghislandi@gssi.it>
2021-05-29 09:39:29 +0200

    Added cycle for 4.2.1

--- Codes/Exercise4/Exercise4_2.cpp
+++ Codes/Exercise4/Exercise4_2.cpp
@@ -58,17 +58,17 @@ int round(int nDice, int &nAttacker, int &nDefender){
             nAttacker --;
         }
         if(nAttacker == 0){
-            cout << "The attacker has no more army :(" << endl;
-            printResults(nAttacker,  nDefender,  attackerWin,  attackerLost);
+            //cout << "The attacker has no more army :(" << endl;
+            //printResults(nAttacker,  nDefender,  attackerWin,  attackerLost);
             return 1;
         }
         if(nDefender == 0){
-            cout << "You conquered the Kamchatka!!!" << endl;
-            printResults(nAttacker,  nDefender,  attackerWin,  attackerLost);
+            //cout << "You conquered the Kamchatka!!!" << endl;
+            //printResults(nAttacker,  nDefender,  attackerWin,  attackerLost);
             return 2;
         }
     }
-    printResults(nAttacker, nDefender, attackerWin, attackerLost);
+    //printResults(nAttacker, nDefender, attackerWin, attackerLost);
     return 0;
 }
 
@@ -77,24 +77,56 @@ int conquerAttempt(int nDice, int &nAttacker, int &nDefender){
     int roundCounter = 1;
     int flag = 0;
     while(flag == 0){
-        cout << "\n***Round " << roundCounter << "***\n";
+        //cout << "\n***Round " << roundCounter << "***\n";
         flag = round(nDice, nAttacker, nDefender);
+        //cout << flag << endl;
         roundCounter ++;
     }
-    cout << "\n*********FINAL RESULT*********\n" << flag << endl;
+    //cout << "\n*********FINAL RESULT*********\n" << flag << endl;
     return flag;
 }
 
 int main(){
+    TApplication * App = new TApplication("T",0,NULL);
+    TCanvas * c = new TCanvas();
 
     int nDice = 3;
-    int nDefender = 23;
-    int nAttacker = 456;
-    int conquerResult = 0;
+    int nDefenderStart = 3;
+    int conquerResult;
 
-    cout << "Starting Point" << endl;
-    cout << "Attacker: " << nAttacker << "\t Defender: " << nDefender << endl;
+    //4.2.1
+    int nAttackerStart;
+    int nAttackerMin = 1;
+    int nAttackerMax = 25;
+    int nAttacker, nDefender;
+    int nConquerAttempt = 500;
+    int nBin = nAttackerMax-nAttackerMin + 1;
 
-    conquerResult = conquerAttempt(nDice, nAttacker, nDefender);
+    TH1F * h = new TH1F("", "", nBin, (double)nAttackerMin-0.5, (double)nAttackerMax+0.5);
+    for(int i=nAttackerMin; i<nAttackerMax; i++){
+        nAttackerStart = i;
+        for(int j=0; j<nConquerAttempt; j++){
+            nDefender = nDefenderStart;
+            nAttacker = nAttackerStart;
+            conquerResult = 0;
+            conquerResult = conquerAttempt(nDice, nAttacker, nDefender);
+            if(conquerResult==2){
+                h->Fill(nAttackerStart);
+            }
+        }
+    }
+    
+    double scaleFactor = (double) 100. / nConquerAttempt;
+    h->Scale((double) scaleFactor);
+
+    c->cd();
+    h->SetLineColor(9);
+    h->SetLineWidth(2);
+    h->SetFillStyle(3003);
+    h->SetFillColor(4);
+    h->Draw("histo");
+        
+    
+    App->Run();
     return 0;
 }
