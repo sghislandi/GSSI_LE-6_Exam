b8d86c1190c94cb9a6dbb6cd20b509ccd45fa548
Stefano Ghislandi <stefano.ghislandi@gssi.it>
2021-05-30 11:09:17 +0200

    Initialized Exercise5.1

---
+++ Codes/Exercise5/Exercise5_1.cpp
@@ -0,0 +1,25 @@
+  //c++ -o  Exercise5_1 Exercise5_1.cpp `root-config --glibs --cflags`
+
+#include <time.h>
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <random>
+
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TAxis.h"
+#include "TH1F.h"
+#include "TStyle.h"
+#include "TLine.h"
+
+using namespace std;
+
+int main(){
+
+
+
+
+  return 0;
+}
