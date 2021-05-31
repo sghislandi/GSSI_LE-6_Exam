db89011df67365f81313daa2e3d1c4ffaa954c89
Stefano Ghislandi <stefano.ghislandi@gssi.it>
2021-05-31 10:07:08 +0200

    Added Exercise5.2

---
+++ Codes/Exercise5/Exercise5_2.cpp
@@ -0,0 +1,18 @@
+//c++ -o  Exercise5_2 Exercise5_2.cpp `root-config --glibs --cflags`
+
+#include <time.h>
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <fstream>
+#include <random>
+
+using namespace std;
+
+int main(){
+
+
+
+
+    return 0;
+}
