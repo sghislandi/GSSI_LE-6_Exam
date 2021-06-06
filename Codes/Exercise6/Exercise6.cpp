301b68133071444aa04ff37515c47e3b3e4d4b44
Stefano Ghislandi <stefano.ghislandi@gssi.it>
2021-06-06 08:47:20 +0200

    Finished Exercise6

--- Codes/Exercise6/Exercise6.cpp
+++ Codes/Exercise6/Exercise6.cpp
@@ -1,1 +1,73 @@
- 
+// c++ -o  Exercise6 Exercise6.cpp `root-config --glibs --cflags`
+
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <fstream>
+
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TMultiGraph.h" 
+#include "TGraph.h"
+#include "TAxis.h"
+
+using namespace std;
+
+
+float harmonicSumFloat(int nSum){
+    float sum = 0;
+    for(int i=1; i<= nSum; i++){sum += (float)1/i;}
+    return sum;
+}
+
+double harmonicSumDouble(int nSum){
+    double sum = 0;
+    for(int i=1; i<= nSum; i++){sum += (double)1/i;}
+    return sum;
+}
+
+int main(){
+    TApplication * App = new TApplication("App",0,NULL);
+
+    vector<int> nSums = {1000,10000,100000,1000000};
+    vector<float> outcomeFloat;
+    vector<float> outcomeDouble;
+    for(int i=0; i<nSums.size(); i++){
+        outcomeFloat.push_back(harmonicSumFloat(nSums[i]));
+        outcomeDouble.push_back(harmonicSumDouble(nSums[i]));
+    }
+
+
+    //Print results
+    cout << "*******RESULTS*******" << endl;
+    cout << "N \t Float \t Double" << endl;
+    for(int i=0; i<nSums.size(); i++){
+        cout << nSums[i] << "\t" << outcomeFloat[i] << "\t" << outcomeDouble[i] << endl;
+    }
+
+    //Plot section 
+    TCanvas * c = new TCanvas();
+    TGraph * g = new TGraph();
+    for(int i=0; i<nSums.size(); i++){
+        g->SetPoint(i, nSums[i], outcomeDouble[i]-outcomeFloat[i]);
+    }
+    c->cd();
+    c->SetLogx();
+    g->SetMarkerColor(2);
+    g->SetMarkerStyle(8);
+    g->SetMarkerSize(1.5);
+    g->GetXaxis()->SetTitle("N");
+    g->GetXaxis()->SetTitleSize(0.05);
+    g->GetXaxis()->SetTitleOffset(0.85);
+    g->GetXaxis()->SetLabelSize(0.04);
+    g->GetYaxis()->SetTitle("Double - Float");
+    g->GetYaxis()->SetTitleSize(0.05);
+    g->GetYaxis()->SetTitleOffset(1.1);
+    g->GetYaxis()->SetLabelSize(0.04);
+    g->Draw("AP");
+
+    c->SaveAs("Difference.pdf");
+    App->Run();
+    
+    return 0;
+}
