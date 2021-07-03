c2a9ff035436e8f8998f5aff51ead859ba218d2a
Stefano Ghislandi <stefano.ghislandi@gssi.it>
2021-07-03 11:32:21 +0200

    Added arithmetic method

--- src/Exercise5/Exercise5_1.cpp
+++ src/Exercise5/Exercise5_1.cpp
@@ -50,6 +50,17 @@ double HitOrMiss(double xMin, double xMax, double yMin, double yMax, int degree,
   return (double) integral / nExtraction;
 }
 
+double AritmethicMean(double xMin, double xMax, int degree, int nExtraction){
+  double integral = 0;
+  random_device initSeed;
+  mt19937 randomSeed(initSeed());    
+  uniform_real_distribution<double> uniformExtractorX(xMin, xMax);
+  for(int i=0; i<nExtraction; i++){
+    integral += functionPower(uniformExtractorX(randomSeed), degree);
+  }
+  return (double) integral / nExtraction;
+}
+
 
 int main(){
   TApplication * App = new TApplication("T",0,NULL);
@@ -70,16 +81,30 @@ int main(){
   TGraph ** graphError;
   graphError = new TGraph*[degreeMax];
 
+  TCanvas * cMean = new TCanvas();
+  TGraph ** graphErrorMean;
+  graphErrorMean = new TGraph*[degreeMax];
+
   //MC looping over degrees and nExtraction
   for(int i=0; i<degreeMax; i++){
-    cout << "Doing the HitOrMiss for x^" << i+1 << endl;
+    cout << "Doing x^" << i+1 << endl;
     graphError[i] = new TGraph();
+    graphErrorMean[i] = new TGraph();
     exactIntegralValue = exactIntegral(i+1);
     counter = 0;
+    clock_t cstart1 = clock();
     for(int j=nExtractionMin; j<=nExtractionMax; j *= 2){
       graphError[i]->SetPoint(counter, j, abs(HitOrMiss(xMin, xMax, yMin, yMax, i+1, j) - exactIntegralValue));
       counter ++;
     }
+    clock_t cstop1 = clock();
+    clock_t cstart2 = clock();
+    for(int j=nExtractionMin; j<=nExtractionMax; j *= 2){
+      graphErrorMean[i]->SetPoint(counter, j, abs(AritmethicMean(xMin, xMax, i+1, j) - exactIntegralValue));
+      counter ++;
+    }
+    clock_t cstop2 = clock();
+    cout << "H or M: " << (double) (cstop1-cstart1) / CLOCKS_PER_SEC << "seconds   Arithmetic mean: " << (double) (cstop2-cstart2) / CLOCKS_PER_SEC << " seconds" << endl;
   }
 
   //Plot section
@@ -92,6 +117,9 @@ int main(){
     graphError[i]->SetMarkerColor(1);
     graphError[i]->SetMarkerStyle(8);
     graphError[i]->SetMarkerSize(0.9);
+    graphErrorMean[i]->SetMarkerColor(2);
+    graphErrorMean[i]->SetMarkerStyle(8);
+    graphErrorMean[i]->SetMarkerSize(0.9);
     pad = (TPad*) c->FindObject(Form("c1_%i", i+1));
     pad->SetLogy();
     pad->SetLogx();
@@ -105,6 +133,7 @@ int main(){
     graphError[i]->GetYaxis()->SetRangeUser(1e-4, 1);
     graphError[i]->GetYaxis()->SetLabelSize(0.06);
     graphError[i]->Draw("AP");
+    graphErrorMean[i]->Draw("same");
   }
   c->SaveAs("figs/Exercise5/Discrepancies.pdf");
 
